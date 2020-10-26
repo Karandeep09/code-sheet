9be7b9dd579423f2a513a060800d633e52b78029
jojo9910 <jyotiyadav943349@gmail.com>
2020-10-26 12:41:33 +0530

    updated union set

--- Algorithms/Minimum Spanning Tree/Kruskal_Algorithm.cpp
+++ Algorithms/Minimum Spanning Tree/Kruskal_Algorithm.cpp
@@ -3,11 +3,15 @@ using namespace std;
 #define ll long long 
 const int Max = 1e5 + 5;
 pair <ll, pair<int, int> > p[Max];
-int root[Max], nodes, edges;
+int root[Max];
+int size[Max];
+int nodes, edges;
 
 void initialize(){
-    for(int i = 0;i < Max;++i)
+    for(int i = 0;i < Max;++i){
         root[i] = i;
+        size[i]=1;
+    }
 }
 
 int find_root(int x){
@@ -17,12 +21,16 @@ int find_root(int x){
     return root[x];
 }
 
-void union_set(int u, int v)
-{
+void union_set(int u, int v){
     int root_u = find_root(u);
     int root_v = find_root(v);
-    if(root_u!=root_v)
-      root[root_u]=root_v;
+    if(root_u!=root_v){
+   if(size[root_u]<size[root_v]){
+   int temp=root_u; root_u=root_v; root_v=temp;
+}
+  root[root_v]=root_u;
+  size[root_u]+=size[v];
+}
 }
 
 ll kruskal_Algo()
--- Data Structure/DSU.cpp
+++ Data Structure/DSU.cpp
@@ -3,12 +3,15 @@ using namespace std;
 #define ll long long 
 const int Max=1e5+5;
 int root[Max];
+int size[Max];
 int nodes,edges;
 
 void initialize()
 {
-    for(int i = 0;i < Max;++i)
+    for(int i = 0;i < Max;++i){
         root[i] = i;
+        size[i]=1;
+    }
 }
 
 int find_root(int x){
@@ -19,8 +22,13 @@ int find_root(int x){
 void union_set(int u, int v){
 int root_u=find_root(u);
 int root_v=find_root(v);
-if(root_u!=root_v)
-root[root_u]=root_v;
+if(root_u!=root_v){
+if(size[root_u]<size[root_v]){
+int temp=root_u; root_u=root_v; root_v=temp;
+}
+root[root_v]=root_u;
+size[root_u]+=size[v];
+}
 }
 
 int main() {
